7efb8a0790a9e51ed4fc53540fdcdd37fb882ffb
Sandhya devi Saminathan <sandhya021999@gmail.com>
2023-09-15 14:26:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import Leaddetails from './components/lead/leaddetails';
 import Editlead from './components/lead/editlead';
 import Servicedetails from './components/service/servicedetails';
 import Editservice from './components/service/editservice';
+import Portal from './portal/Portal';
 
 
 
@@ -47,28 +48,27 @@ function App() {
                 <Route path='/' element={<Login />}></Route>
                 <Route path='/register' element={<Register />}></Route>
                 <Route path='Home/' element={<Home />} />
-                <Route path='Userdata/:id' element={<Userdata />} />
-                <Route path='/lead' element={<Lead />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/rservice' element={<RequestService />} />
-                <Route path='/service' element={<Service />} />
-                <Route path='user/:id' element={<Profile />} />
-                <Route path='/userlist' element={<UserList />} />
-                <Route path='/edit/:id' element={<EditUser />} />
-                <Route path='/Addemployee' element={<Addemployee />} />
-                <Route path='lead' element={<Lead />} />
+                <Route path='/portal' element={<Portal />}>
+                  <Route path='Userdata/:id' element={<Userdata />} />
+                  <Route path='/lead' element={<Lead />} />
+                  <Route path='/contact' element={<Contact />} />
+                  <Route path='/rservice' element={<RequestService />} />
+                  <Route path='/service' element={<Service />} />
+                  <Route path='user/:id' element={<Profile />} />
+                  <Route path='/userlist' element={<UserList />} />
+                  <Route path='/edit/:id' element={<EditUser />} />
+                  <Route path='/Addemployee' element={<Addemployee />} />
+                  <Route path='lead' element={<Lead />} />
+                  <Route path='leaddetails/:id' element={<Leaddetails />} />
+                  <Route path='/editlead/:id' element={<Editlead />} />
+                  <Route path='servicedetails/:id' element={<Servicedetails />} />
+                  <Route path='/editservice/:id' element={<Editservice />} />
+                </Route>
                 <Route path='/reset' element={<ResestPassword />} />
                 <Route path='/ForgetPassword' element={<ForgetPassword />} />
                 <Route path='/Verification/:id' element={<Verification />} />
                 <Route path='/ChangePassword/:id' element={<ChangePassword />} />
-                <Route path='leaddetails/:id' element={<Leaddetails />} />
-                <Route path='/editlead/:id' element={<Editlead />} />
-                <Route path='servicedetails/:id' element={<Servicedetails />} />
-                <Route path='/editservice/:id' element={<Editservice />} />
-
               </Routes>
-
-
             </BrowserRouter>
           </div>
         </div>
